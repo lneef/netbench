51cbc4954f24fd815ae26c8b4b6ad0b807427daa
Lukas Neef <lukas.neef@tum.de>
2025-12-18 14:40:24 +0100

    test pps

--- rudp.cc
+++ rudp.cc
@@ -28,7 +28,6 @@
 #include "packet.h"
 #include "port.h"
 #include "rudp.h"
-#include "statistics.h"
 #include "util.h"
 
 void send_rudp(void *port) {
@@ -58,14 +57,9 @@ void send_rudp(void *port) {
       tx_free = 0;
     tx_nb = rudp_peer.submit_tx_burst(std::span(start_it + tx_free, end_it));
     tx_free += tx_nb;
-    uint16_t nb_rx = 0;
-    do {
-      nb_rx += rudp_peer.submit_rx_burst(rpkts);
-    } while (nb_rx < tx_nb);
-    rte_pktmbuf_free_bulk(rpkts.data(), nb_rx);
-    tb.per_thread_submit_stat.submitted += tx_nb;
+    auto rx_nb = rudp_peer.submit_rx_burst(rpkts);
+    rte_pktmbuf_free_bulk(rpkts.data(), rx_nb);
   }
-  rudp_peer.make_progress();
   auto stats = rudp_peer.get_stats();
   std::cout << std::format(
                    "acked: {}, retransmitted: {}, sent: {}, rtt: {:2}\n",
@@ -86,29 +80,15 @@ void recv_rudp(void *port) {
                  tb.send_pool,
                  pg};
 
-  uint16_t tx_free = config.burst_size, tx_nb;
-  uint16_t queued = 0;
+  uint16_t tx_free = config.burst_size;
   std::vector<pkt_t *> pkts(tx_free);
   std::vector<pkt_t *> rpkts(tx_free);
-  auto start_it = pkts.begin();
   uint64_t cycles = rte_get_timer_cycles();
   uint64_t end = config.rtime * rte_get_timer_hz() + cycles;
   for (; cycles < end; cycles = rte_get_timer_cycles()) {
     auto nb_rx = rudp_peer.submit_rx_burst(pkts);
-    for (auto *pkt : std::span(start_it, start_it + nb_rx)) {
-      pg.packet_pong_ctor(pkt);
-      rpkts[queued++] = pkt;
-    }
-
-    tx_nb = 0;
-    if (queued)
-      tx_nb = rudp_peer.submit_tx_burst(
-          std::span(rpkts.begin(), rpkts.begin() + queued));
     rudp_peer.make_progress();
-    for (uint16_t i = tx_nb, j = 0; i < nb_rx; ++i)
-      rpkts[j++] = rpkts[i];
-    tb.per_thread_submit_stat.submitted += tx_nb;
-    queued -= tx_nb;
+    rte_pktmbuf_free_bulk(pkts.data(), nb_rx);
   }
 }
 
@@ -122,11 +102,11 @@ int main(int argc, char *argv[]) {
   lcore_adapter adapter{info, config};
 
   switch (config.role) {
-  case opmode::PING: {
+  case opmode::FORWARD: {
     send_rudp(&adapter);
     break;
   }
-  case opmode::PONG: {
+  case opmode::RECEIVE: {
     recv_rudp(&adapter);
     break;
   }
